5bcb3068cc92aae23c947914c134f9abf540d905
Farepal <repalfarel@gmail.com>
2023-05-23 01:26:08 +0700

    mantappp

--- tes.cpp
+++ tes.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int x = 0;
-    if (++x && 1)
-        cout << "YES" << endl;
-    cout << x << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i == j)
+                cout <<  << "\t";
+            else
+                cout << 0 << "\t";
+        }
+        cout << endl;
+    }
+
+    string s;
+    cin >> s;
+    int indX = s.find("x");
+    
+
+    // int i = 0, j = 0;
+    // do {
+    //     do {
+    //         cout << rand() << "\t";
+
+    //     } while (j < 3)
+    //     cout << endl;
+    //     i++;
+    // } while (i < 3)
 }
