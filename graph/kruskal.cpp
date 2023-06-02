b6d462f23509f0842a46e36c45b52648966e2467
Farepal <repalfarel@gmail.com>
2023-06-03 00:27:20 +0700

    gasss

---
+++ graph/kruskal.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define jumlahVertex 7
+int adjacencyMatrix[jumlahVertex][jumlahVertex] = {
+    {0, 2, 4, 1, 0, 0, 0},
+    {2, 0, 0, 3, 10, 0, 0},
+    {4, 0, 0, 2, 5, 0, 0},
+    {1, 3, 2, 0, 7, 8, 4},
+    {0, 10, 5, 7, 0, 0, 6},
+    {0, 0, 0, 8, 0, 0, 1},
+    {0, 0, 0, 4, 6, 1, 0}};
+
+struct OneEdges
+{
+    int sourceVertex, destinationVertex, weight;
+};
+
+bool comparisonOfEdges (OneEdges A, OneEdges B)
+{
+    return (A.weight < B.weight);
+}
+
+vector<OneEdges> graph;//graph awal
+vector <OneEdges> MST;//graph hasil MST
+int jumlahEdges;//jumlah garis
+int root[jumlahVertex];
+
+int find_root(int numberOfVertex)
+{
+    if (root[numberOfVertex] == numberOfVertex)
+    {
+        return numberOfVertex;
+    }
+    return find_root(root[numberOfVertex]);
+}
+
+bool checkCycle(int sVertex, int dVertex)
+{
+    if (find_root(sVertex) == find_root(dVertex))
+        return false;
+    return true;
+}
+
+void unionSubtree(int rootOfSubtree1, int rootOfSubtree2)
+{
+    root[rootOfSubtree1] = root[rootOfSubtree2];
+}
+
+int main()
+{
+    for (int i = 0; i < jumlahVertex; i++)
+    {
+        for (int j = i + 1; j < jumlahVertex; j++)
+        {
+            if (adjacencyMatrix[i][j] != 0)
+            {
+                OneEdges temp;
+                temp.sourceVertex = i;
+                temp.destinationVertex = j;
+                temp.weight = adjacencyMatrix[i][j];
+                graph.push_back(temp);
+            }
+        }
+    }
+
+    //Jumlah Edges
+    jumlahEdges = graph.size();
+
+    ////////////KRUSKAL ALGORITHM////////////////////////
+    sort (graph.begin(), graph.end(), comparisonOfEdges);
+
+    //DECLARE array ROOT untuk
+    for (int i = 0; i < jumlahVertex; i++)
+        root[i] = i;
+    for (int i = 0; i < jumlahEdges; i++)
+        cout << "v" << graph[i].sourceVertex + 1 << " -> v" << graph[i].destinationVertex + 1 << " " << graph[i].weight << endl;
+    for (int i = 0; i < jumlahVertex; i++)
+        cout << root[i] << " ";
+    cout << endl;
+    for (int i = 0; i < jumlahEdges; i++)
+    {
+        if (checkCycle(graph[i].sourceVertex, graph[i].destinationVertex))
+        {
+            int rootOfVertex1 = find_root(graph[i].sourceVertex);
+            int rootOfVertex2 = find_root(graph[i].destinationVertex);
+            //vertex destinasi biasanya bakal menjadi root di subtree
+            MST.push_back(graph[i]);
+            unionSubtree(rootOfVertex1, rootOfVertex2);
+        }
+    }
+    cout << MST.size() << endl;
+
+    for (int i = 0; i < MST.size(); i++)
+        cout << "v" << MST[i].sourceVertex + 1 << " -> v" << MST[i].destinationVertex + 1 << " " << MST[i].weight << endl;
+    return 0;
+}
